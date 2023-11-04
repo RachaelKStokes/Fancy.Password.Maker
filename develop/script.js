bd2c93276fd645404864cc8786ae1357441b5c19
unknown <rachael.kate.stokes@gmail.com>
2023-11-04 13:24:05 -0400

    first attempt. added prompts for character types, put in a message for meeting criteria

--- develop/script.js
+++ develop/script.js
@@ -1,6 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  //all code goes in this block
+  //password character variables
+  var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+  var upperCase = lowerCase.toUpperCase();
+  var numbers = '0123456789';
+  var specialCharacters = '!@#$%^&*?';
+  
+
+  //prompts for password criteria
+  var lowerCaseQ = confirm("Would you like to use lowercase letters?");
+  var upperCaseQ = confirm("Would you like to use uppercase letters?");
+  var numbersQ = confirm("Would you like to use numbers?");
+  var specialCharactersQ = confirm("Would you like to use special characters?");
+
+ //following criteria
+ if (lowerCaseQ === false && upperCaseQ === false && numbersQ === false && specialCharactersQ === false) {
+  return "You must select at least one character type to meet password criteria.";
+  };
+
+  //generating passwords
+  if(lowerCaseQ === true && upperCaseQ === false && numbersQ === false && specialCharactersQ === false) {
+    
+  }
+
+
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
