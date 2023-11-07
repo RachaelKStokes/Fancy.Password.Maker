8b0c982121ede9697ed2d0a6d48e15b634561a8f
unknown <rachael.kate.stokes@gmail.com>
2023-11-06 19:07:30 -0500

    turned off prompts for now while I work on the randomizing and including of different character types.  Also added if statements for making a random character pool

--- develop/script.js
+++ develop/script.js
@@ -12,9 +12,9 @@ function generatePassword() {
   var numbers = '0123456789';
   var specialCharacters = '!@#$%^&*?';
   var lengthInput = 10;
-  //booleans
-  var hasLowercase = false;
-  var hasUppercase = false;
+  //booleans for inputs
+  var hasLowercase = true;
+  var hasUppercase = true;
   var hasNumeric = false;
   var hasSpecial = false;
 
@@ -22,16 +22,36 @@ function generatePassword() {
    //prompts for password criteria, input required between 8 and 28 characters
    //All input should be validated
 
-   var lowerCaseQ = confirm("Would you like to use lowercase letters?");
-   var upperCaseQ = confirm("Would you like to use uppercase letters?");
-   var numbersQ = confirm("Would you like to use numbers?");
-   var specialCharactersQ = confirm("Would you like to use special characters?");
+   //!! means or ! means true
+   if (!hasLowercase !! !hasUppercase) {
+    alert("Please choose at least one character type.");
+    return;
+   }
+
+  //possible characters pool builder for generating random password
+   if (hasLowercase) {
+    possibleCharacters += lowerCase;
+   }
+
+   if (hasUppercase) {
+    possibleCharacters += upperCase;
+   }
+
+   if (hasSpecial) {
+    possibleCharacters += specialCharacters;
+   }
+
+   //prompt q's
+   //var lowerCaseQ = confirm("Would you like to use lowercase letters?");
+   //var upperCaseQ = confirm("Would you like to use uppercase letters?");
+   //var numbersQ = confirm("Would you like to use numbers?");
+   //var specialCharactersQ = confirm("Would you like to use special characters?");
 
   //password should be generated and displayed
 
   for (var i = 0; i < lengthInput; i++){
-    var random = Math.floor(math.random() * lowerCase.length);
-    password += lowerCase[random];
+    var random = Math.floor(math.random() * possibleCharacters.length);
+    password += possibleCharacters[random];
   }
    console.log("password:");
    return "password";
